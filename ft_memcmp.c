8e160795d3bc1f8193c04c47b2cf70db06535fde
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2024-11-04 23:25:29 -0300

    unit test memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 22:04:38 by lukorman          #+#    #+#             */
-/*   Updated: 2024/10/25 20:50:19 by lukorman         ###   ########.fr       */
+/*   Updated: 2024/11/04 23:22:08 by lukorman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	i = 0;
-	while (i < n && (str1[i] != '\0' || str2[i] != '\0'))
+	while (i < n)
 	{
 		if (str1[i] != str2[i])
 			return (str1[i] - str2[i]);
