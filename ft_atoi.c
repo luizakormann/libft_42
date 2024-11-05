74ac0c66808b040f6fd5ba25b1e1e5b1c70ca325
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2024-11-04 21:39:49 -0300

    francinette corrections atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,23 +6,13 @@
 /*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 23:14:39 by lukorman          #+#    #+#             */
-/*   Updated: 2024/10/25 20:49:02 by lukorman         ###   ########.fr       */
+/*   Updated: 2024/11/04 21:29:15 by lukorman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	spaces(const char *nptr)
-{
-	size_t	i;
-
-	i = 0;
-	while (nptr[i] == ' ')
-	{
-		i++;
-	}
-	return (i);
-}
+int	spaces(const char *nptr);
 
 int	ft_atoi(const char *nptr)
 {
@@ -35,18 +25,29 @@ int	ft_atoi(const char *nptr)
 	res = 0;
 	num = 0;
 	sig = 1;
-	if (nptr[i] == '-')
+	if (nptr[i] == '-' || nptr[i] == '+')
 	{
-		sig *= -1;
+		if (nptr[i] == '-')
+			sig *= -1;
 		i++;
+		if (nptr[i] == '-' || nptr[i] == '+')
+			return (0);
 	}
-	if (ft_isdigit(nptr[i]) == 0)
-		return (0);
-	while (ft_isdigit(nptr[i]) != 0 && nptr[i] != '\0')
+	while (ft_isdigit(nptr[i]) != 0 && nptr[i])
 	{
 		num = nptr[i] - 48;
 		res = res * 10 + num;
 		i++;
 	}
 	return (res * sig);
+}
+
+int	spaces(const char *nptr)
+{
+	size_t	i;
+
+	i = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	return (i);
 }
