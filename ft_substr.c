25ed83ad565d78d05df0eb72351f54a51897a202
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2024-11-04 23:45:37 -0300

    libfttester corrections substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 20:46:29 by lukorman          #+#    #+#             */
-/*   Updated: 2024/11/04 22:52:16 by lukorman         ###   ########.fr       */
+/*   Updated: 2024/11/04 23:41:24 by lukorman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	slen;
 	size_t	i;
 
-	res = (char *)malloc(len + 1);
-	slen = ft_strlen(s);
-	i = 0;
 	if (!s)
 		return (NULL);
-	if (!res)
-		return (NULL);
-	if (len > slen - start)
-		len = slen - start;
+	slen = ft_strlen(s);
 	if (start >= slen)
 		return (ft_strdup(""));
+	if (len > slen - start)
+		len = slen - start;
+	res = (char *)malloc(len + 1);
+	i = 0;
+	if (!res)
+		return (NULL);
 	while (i < len)
 	{
 		res[i] = s[start + i];
