536612d5f66562ddecfd79971024df6c71c907ac
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2024-11-04 22:54:57 -0300

    francinette corrections substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 20:46:29 by lukorman          #+#    #+#             */
-/*   Updated: 2024/11/04 21:40:25 by lukorman         ###   ########.fr       */
+/*   Updated: 2024/11/04 22:52:16 by lukorman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	res = (char *)malloc(len + 1);
 	slen = ft_strlen(s);
 	i = 0;
+	if (!s)
+		return (NULL);
 	if (!res)
 		return (NULL);
+	if (len > slen - start)
+		len = slen - start;
+	if (start >= slen)
+		return (ft_strdup(""));
 	while (i < len)
 	{
 		res[i] = s[start + i];
