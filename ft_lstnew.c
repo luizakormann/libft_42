41faba3d3d2d9ed1eca9487d322d9ab2763f333c
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2024-11-11 16:33:22 -0300

    lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 16:24:53 by lukorman          #+#    #+#             */
+/*   Updated: 2024/11/11 16:28:22 by lukorman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if(!node)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
--- libft.h
+++ libft.h
@@ -59,4 +59,6 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
+t_list	*ft_lstnew(void *content);
+
 #endif
