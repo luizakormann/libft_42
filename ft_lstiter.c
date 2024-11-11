cd7026ea46dc07ef73a09565334841abbdd5c9f1
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2024-11-11 19:00:10 -0300

    francinette correction

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:35:34 by lukorman          #+#    #+#             */
-/*   Updated: 2024/11/11 17:40:32 by lukorman         ###   ########.fr       */
+/*   Updated: 2024/11/11 18:59:13 by lukorman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 	t_list	*tmp;
 
 	tmp = lst;
-	while (!tmp)
+	while (tmp)
 	{
 		f(tmp->content);
 		tmp = tmp->next;
