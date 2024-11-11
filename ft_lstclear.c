5ce65c15190b73efb31cf12747d9eeaf0fb4a922
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2024-11-11 17:34:50 -0300

    lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 17:23:51 by lukorman          #+#    #+#             */
+/*   Updated: 2024/11/11 17:34:35 by lukorman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		free(*lst);
+		*lst = tmp;
+	}
+	tmp = NULL;
+}
