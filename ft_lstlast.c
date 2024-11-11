fd77a6c3a0aeb86b01466ad283bdc7b068f74fe1
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2024-11-11 18:10:00 -0300

    francinette correction

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 16:53:15 by lukorman          #+#    #+#             */
-/*   Updated: 2024/11/11 16:58:34 by lukorman         ###   ########.fr       */
+/*   Updated: 2024/11/11 18:09:07 by lukorman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstlast(t_list *lst)
 	t_list	*tmp;
 
 	tmp = lst;
+	if (!tmp)
+		return (NULL);
 	while (tmp->next)
 	{
 		tmp = tmp->next;
