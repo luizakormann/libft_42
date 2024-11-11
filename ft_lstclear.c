48ddd277bcf2973bc94c1a874d8f1a4944a013c8
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2024-11-11 18:58:43 -0300

    francinette correction

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:23:51 by lukorman          #+#    #+#             */
-/*   Updated: 2024/11/11 17:34:35 by lukorman         ###   ########.fr       */
+/*   Updated: 2024/11/11 18:57:51 by lukorman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp;
 
+	if (!lst || !del)
+		return ;
 	while (*lst)
 	{
 		tmp = (*lst)->next;
-		ft_lstdelone(*lst, del);
+		del((*lst)->content);
 		free(*lst);
 		*lst = tmp;
 	}
-	tmp = NULL;
+	*lst = NULL;
 }
